e061042b2f3460257536f66707b9f897b5363103
oscar066 <oscarkaruga1@gmail.com>
2023-08-09 08:00:29 +0300

    :art: improve code organisation

--- KarenRead.py
+++ KarenRead.py
@@ -1,19 +1,24 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfdevice import PDFDevice
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
-from io import StringIO
-import os
-import colorama
 from colorama import Fore, Back, Style
+from io import StringIO
+from PIL import Image
+import subprocess
+import colorama
 import pyfiglet
+import os
 
-def pdfReader(file='AIS_assignment.pdf', output_file='output.txt'):
-    # Open the PDF file for reading
-    
+def intro_style_header_text():
+
+    print("\t==============================================")
+    print(Fore.BLUE + "\tWelcome to Oscar's Text to Speech Program")
+    print("\t==============================================\n")
+
     def generate_ascii_art(text, font='standard'):
         ascii_art = pyfiglet.figlet_format(text, font=font, justify='center')
         return ascii_art
@@ -25,29 +30,32 @@ def pdfReader(file='AIS_assignment.pdf', output_file='output.txt'):
 
     print(Fore.CYAN + ascii_art)
 
-    print("\t==============================================")
-    print(Fore.BLUE + "\tWelcome to Oscar's Text to Speech Program")
-    print("\t==============================================\n")
+def detect_source_format(file):
 
-    print("\tPlease enter the path of the PDF file you would like to read:")
-    file = input("\tEnter the path: ")
+    print("Please enter the path of the PDF file you would like to read:")
+    file = input("Enter the path: ")
 
-    def is_pdf(file):
-        if file[-4:] == '.pdf':
-            return True
-        else:
-            return False
+    if file[-4:] == '.pdf':
+        pdfReader(file=file, output_file='output.txt')
+        print(Fore.GREEN + "\n\tPDF Text extracted successfully ")
+        text_to_speech(file='output.txt')
 
-    try:
-        if is_pdf(file):
-            print(Fore.GREEN + "\n\tText extracted successfully ")
-        else:
-            raise Exception("\tFile is not a PDF")
-    except Exception as e:
-        print(Fore.RED + "\tError:", e)
-        print(Fore.RED + "\tPlease enter a valid PDF file path")
+    elif file[-4:] == '.png' or file[-4:] == '.jpg':
+        print(Fore.GREEN + "\n\tImage Text extracted successfully ")
+        tesseractOCR(file=file, output_file='output.txt')
+        text_to_speech(file='output.txt')
+
+    elif file[-4:] == '.txt':
+        print(Fore.GREEN + "\n\tText File detected")
+        text_to_speech(file=file)
+        
+    else:
+        print(Fore.RED + "\tError: File is not a PDF or Image")
         print(Fore.RED + "\tExiting program...")
 
+
+def pdfReader(file='AIS_assignment.pdf', output_file='output.txt'):
+    # Open the PDF file for reading
     fp = open(file, 'rb')
     parser = PDFParser(fp)
     # Create a PDF document using the parser
@@ -72,6 +80,22 @@ def pdfReader(file='AIS_assignment.pdf', output_file='output.txt'):
 
     return output_file
 
+def tesseractOCR(file='E-com_extract.png', output_file='output.txt'):
+
+    image = image.point(lambda x: 0 if x < 135 else 255)
+    image.save(newFilePath)
+
+    # call tesseract to do OCR on the newly created image
+    subprocess.call(['tesseract',newFilePath,'output'])
+
+    # Open and read the resulting data file
+    outputFile = open('output.txt', 'r')
+    print(outputFile.read())
+    outputFile.close()
+
+    return output_file
+
+
 def text_to_speech(file=pdfReader()):
 
     print(Fore.CYAN + "\n\tYour File is ready to be read\n")
@@ -83,7 +107,7 @@ def text_to_speech(file=pdfReader()):
     print(Fore.CYAN + "\t4. Victoria")
     print(Fore.CYAN + "\t5. Daniel")
 
-    voice = input(Fore.GREEN + "\tEnter your choice: ")
+    voice = input(Fore.GREEN + "\nEnter your choice: ")
 
     voices_map = {
         '1': 'Karen',
@@ -99,11 +123,14 @@ def text_to_speech(file=pdfReader()):
         print(Fore.RED + "\tInvalid choice! Defaulting to Karen's voice.")
         voice = 'Karen'
 
-    print(Fore.MAGENTA + "\n\tChoose a reading speed from 50 to 300 words per minute:")
-    speed = input(Fore.GREEN + "\tEnter your choice: ")
+    print(Fore.MAGENTA + "\nChoose a reading speed from 50 to 300 words per minute:")
+    speed = input(Fore.GREEN + "Enter your choice: ")
 
     print(Fore.CYAN  + "\n\tReading your file now...\n")
     os.system(f'say -v {voice} -f {file} -r {speed} --quality=127 -i')
 
+    print(Fore.GREEN + "\nThank you for using Oscar's Text to Speech Program\n")
+
 if __name__ == "__main__":
-    text_to_speech()
+    intro_style_header_text()
+    detect_source_format(file='AIS_assignment.pdf')
